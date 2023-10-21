a7ae48948a3367b82a94a2e3ef79be3fd57d7318
OnlyNumber <98099864+OnlyNumber@users.noreply.github.com>
2023-10-21 22:56:50 +0300

    Added rigging and download new animations

--- 3DShooterRogueLike/Assets/Scripts/ThirdPersonShooterController.cs
+++ 3DShooterRogueLike/Assets/Scripts/ThirdPersonShooterController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
+using UnityEngine.Animations.Rigging;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
+    private float _aimWeight;
+
     [SerializeField]
     private CinemachineVirtualCamera _cinemachineVirtualCamera;
 
@@ -29,6 +32,9 @@ public class ThirdPersonShooterController : MonoBehaviour
     [SerializeField]
     private Animator _animator;
 
+    [SerializeField]
+    private Rig _rig;
+
     private void Start()
     {
         _starterAssetsInputs = GetComponent<StarterAssetsInputs>();
@@ -36,6 +42,8 @@ public class ThirdPersonShooterController : MonoBehaviour
 
     private void Update()
     {
+        _rig.weight = Mathf.Lerp(_rig.weight, _aimWeight, Time.deltaTime * 20f);
+
         Vector2 screenPoint = new Vector2(Screen.width/2, Screen.height/2);
         Ray aimRay = Camera.main.ScreenPointToRay(screenPoint);
         if(Physics.Raycast(aimRay,out RaycastHit hit,999f, _aimLayers))
@@ -47,31 +55,41 @@ public class ThirdPersonShooterController : MonoBehaviour
 
         if(_starterAssetsInputs.aim)
         {
-            _cinemachineVirtualCamera.gameObject.SetActive(true);
-            _thirdPersonController.SetSensitivity(_aimSensitivity);
-
-            worldPosition = mousePosition;
-            worldPosition.y = transform.position.y;
-            Vector3 aimDirection = (worldPosition - transform.position).normalized;
-            transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
-
-            _thirdPersonController.SetRotating(false);
-
-            _animator.SetLayerWeight(1, Mathf.Lerp(_animator.GetLayerWeight(1), 1, Time.deltaTime * 10f));
-
+            StartAim();
         }
         else
         {
-            _cinemachineVirtualCamera.gameObject.SetActive(false);
-            _thirdPersonController.BackToNormalSensitivity();
-            _thirdPersonController.SetRotating(true);
-
-            _animator.SetLayerWeight(1, Mathf.Lerp(_animator.GetLayerWeight(1), 0, Time.deltaTime * 10f));
+            StopAim();
 
         }
+    }
 
+    private void StartAim()
+    {
+        _cinemachineVirtualCamera.gameObject.SetActive(true);
+        _thirdPersonController.SetSensitivity(_aimSensitivity);
 
+        worldPosition = mousePosition;
+        worldPosition.y = transform.position.y;
+        Vector3 aimDirection = (worldPosition - transform.position).normalized;
+        transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
 
+        _thirdPersonController.SetRotating(false);
+
+        _animator.SetLayerWeight(1, Mathf.Lerp(_animator.GetLayerWeight(1), 1, Time.deltaTime * 10f));
+
+        _aimWeight = 1;
+    }
+
+    private void StopAim()
+    {
+        _cinemachineVirtualCamera.gameObject.SetActive(false);
+        _thirdPersonController.BackToNormalSensitivity();
+        _thirdPersonController.SetRotating(true);
+
+        _animator.SetLayerWeight(1, Mathf.Lerp(_animator.GetLayerWeight(1), 0, Time.deltaTime * 10f));
+
+        _aimWeight = 0;
     }
 
 }
